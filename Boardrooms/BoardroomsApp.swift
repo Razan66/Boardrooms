3de20963524360026b8f567da92d97769875562b
Razan66 <raldughaythir24@twq.idserve.net>
2025-01-16 11:37:35 +0300

    Initial Commit

---
+++ Boardrooms/BoardroomsApp.swift
@@ -0,0 +1,17 @@
+//
+//  BoardroomsApp.swift
+//  Boardrooms
+//
+//  Created by Razan on 16/07/1446 AH.
+//
+
+import SwiftUI
+
+@main
+struct BoardroomsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
